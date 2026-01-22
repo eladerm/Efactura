2ec62afb108664d07715dfc93286a00f8ed6bbd8
ELADERM GROUP <eladerm.info@gmail.com>
2026-01-22 21:25:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1175223924-14ab3",
+  "appId": "1:25004649082:web:c7cbfad73a77215c4ee9c3",
+  "apiKey": "AIzaSyDsoPbsXpMuAQGvDFidIUHmKdUYktJRSMY",
+  "authDomain": "studio-1175223924-14ab3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "25004649082"
+};
