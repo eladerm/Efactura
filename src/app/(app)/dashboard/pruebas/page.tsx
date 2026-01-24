ecd959b2e8ba7600b7302378e9fd5d5f8bbcc7ab
ELADERM GROUP <eladerm.info@gmail.com>
2026-01-24 18:06:58 +0000

    CardFooter is not defined
    src/app/(app)/dashboard/pruebas/page.tsx (175

--- src/app/(app)/dashboard/pruebas/page.tsx
+++ src/app/(app)/dashboard/pruebas/page.tsx
@@ -3,7 +3,7 @@
 
 import { useState } from 'react';
 import { PageHeader } from '@/components/page-header';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
